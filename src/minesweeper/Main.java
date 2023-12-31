2271593a7249908741103113a971c9968be9b71b
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2023-12-31 17:05:57 -0300

    Initial commit

---
+++ src/minesweeper/Main.java
@@ -0,0 +1,11 @@
+package minesweeper;
+
+public class Main {
+
+	//Fields
+	private static Screen screen;
+	//Constructor
+	public static void main(String[] args) {
+		screen = new Screen();
+	}
+}
---
+++ src/minesweeper/Screen.java
@@ -0,0 +1,14 @@
+package minesweeper;
+
+import javax.swing.JFrame;
+
+public class Screen extends JFrame{
+
+	public Screen() {
+		setLocation(200, 200);
+		setSize(600,600);
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
